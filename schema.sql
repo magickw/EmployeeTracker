02fe705865774f611429944f9bf0602bc75f6b6d
Baofeng Guo <peterguo1983@gmail.com>
2022-01-31 14:24:59 -0800

    initial commit.

---
+++ schema.sql
@@ -0,0 +1,13 @@
+--Creates employee tracker database
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+--Creates department tables,
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
