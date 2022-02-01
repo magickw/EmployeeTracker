c567672efc95883f9c73d02d6139f838cd14c620
Baofeng Guo <peterguo1983@gmail.com>
2022-02-01 15:30:25 -0800

    Updated schema.sql.

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,33 @@ CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
---Creates department tables,
+--Creates department table
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
---
+-- Creates role table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- Creates employee table
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
